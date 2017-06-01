7f5ced0ea1bc24175fc88e888e7d965751ebf0c0
unknown <f.e.h.farshad@gmail.com>
2017-06-01 16:51:37 +0430

    addfiles'

---
+++ admin/js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $(".button-collapse").sideNav(
+        {
+            menuWidth: 245, // Default is 300
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        }
+    );
+    $(".button-collapse-products").sideNav(
+        {
+            menuWidth: 245, // Default is 300
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        }
+    );
+
+    $('.collapsible').collapsible();
+
+    $('.modal').modal();
+
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+    $('select').material_select();
+
+    
+});
+
+
